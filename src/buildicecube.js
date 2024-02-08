102797cf8cb59bbb32c0c7d2ad1423278c864d58
Ed Unverricht <edguy99@hotmail.com>
2024-02-08 16:44:17 -0700

    Update buildicecube.js

--- src/buildicecube.js
+++ src/buildicecube.js
@@ -41,23 +41,6 @@ function buildicecube() {
   cube.material.side = THREE.BackSide;
 
 
-  var labbuildinggeometry = new THREE.BoxGeometry( 60, 60, 150 );
-  var labbuildingmaterial =  new THREE.MeshPhongMaterial( {color: 0x408040}  );
-  var labbuilding = new THREE.Mesh( labbuildinggeometry, labbuildingmaterial );
-  ground.add( labbuilding );
-  labbuilding.position.set(0,30,0);
-
-  var tube1curve = new THREE.QuadraticBezierCurve3(new THREE.Vector3( 0, -30, 0 ), new THREE.Vector3( 0, 0, 0 ), new THREE.Vector3( 0, 30, 0 ))
-  var tube1geometry = new THREE.TubeGeometry(tube1curve, 10, 15, 8, false);
-  var tube1material =  new THREE.MeshPhongMaterial( {color: 0xffffff}  );
-  var tube1 = new THREE.Mesh( tube1geometry, tube1material );
-  ground.add( tube1 );
-  tube1.position.set(0,30,140);
-  var tube2 = tube1.clone();
-  ground.add( tube2 );
-  tube2.position.set(0,30,-140);
-
-
   var detectorgeometry = new THREE.SphereGeometry(6, 12, 6);
   var sensors = stdata.split('\n');
   for (var i=0;i<sensors.length;i++) {
