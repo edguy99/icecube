1b0c40954aef6babb08509380eebf31122951bc9
Ed Unverricht <edguy99@hotmail.com>
2024-02-09 10:46:23 -0700

    Update icecube.js

--- src/icecube.js
+++ src/icecube.js
@@ -6,6 +6,7 @@ var explorescene = new THREE.Scene();
 const explorerenderer = new THREE.WebGLRenderer();
 const explorerenderer3d = new THREE.WebGLRenderer();
 var controls = new THREE.OrbitControls( explorecamera,explorerenderer.domElement );
+var indentcanvas;
 
 var spinner;
 var vrviewers = 1;
